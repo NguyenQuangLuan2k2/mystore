a66508a70a8e18172b115f3407ae9d0717dda639
Luan <luan007@gmail.com>
2025-01-07 10:07:19 +0700

    second commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+  resource :unsubscribe, only: [ :show ]
+end
